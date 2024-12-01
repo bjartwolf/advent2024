a33bf734c59dd8b885f9b96d3c834faeed2e3cbf
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2024-12-01 09:17:42 +0100

    optimized things with a map

--- day_1/Program.fs
+++ day_1/Program.fs
@@ -6,36 +6,30 @@ module Input =
     let readInit (filePath: string): (int*int) list = 
         use sr = new StreamReader (filePath) 
         let lines = sr.ReadToEnd()
-        let numbers = lines.Split(System.Environment.NewLine);
-        let splitLines = numbers |> Array.map(fun line -> line.Split("  ")) 
-        let nums = splitLines |> Array.map(fun strings  -> Int32.Parse(strings.[0]), Int32.Parse(strings.[1]))
-        nums |> Array.toList
-
-    let testData: (int*int) list = [ (3,4); (4,3); (2,5); (1,3); (3,9); (3,3)]
+        lines.Split(System.Environment.NewLine);
+            |> Array.map(fun line -> line.Split("  ")) 
+            |> Array.map(fun strings -> Int32.Parse(strings.[0]), Int32.Parse(strings.[1]))
+            |> Array.toList
 
     let splitAndSortList (pairList: (int*int) list): (int list)*(int list) = 
-        let firstList = pairList |> List.map fst |> List.sort
-        let secondList = pairList |> List.map snd |> List.sort
-        (firstList, secondList)
+        (pairList |> List.map fst |> List.sort, pairList |> List.map snd |> List.sort)
 
     let findDistance (l1: int list,l2: int list) : int =
-        let lst = List.zip l1 l2
-        lst |> List.map (fun (n1,n2) -> abs(n1 - n2))  
-            |> List.sum
+        List.zip l1 l2
+            |> List.sumBy (fun (n1,n2) -> abs(n1 - n2))  
 
-    let countOccurrances (l1: int list, l2: int list): ((int * int) list ) = 
-        l1 |> List.map (fun num -> (num, l2 |> List.filter (fun n2 -> n2 = num) |>  List.length ))
+    let countOccurrances (l1: int list, l2: int list) = 
+        let counts = l2 |> List.countBy id |> Map.ofList
+        l1 |> List.map (fun num1 -> (num1, Map.tryFind num1 counts |> Option.defaultValue 0)) 
 
-    let productOfOccurances (counts: (int*int) list) : int =
-        counts |> List.map (fun (num,count) -> num*count) |> List.sum
-
+    let productOfOccurances (counts: (int*int) list) =  
+        counts |> List.fold(fun acc (num,count) -> acc + num*count) 0
 
     [<Fact>]
     let test () = 
         let input = readInit "input1.txt" 
         Assert.Equal(6, input.Length) 
-        Assert.Equivalent(testData, input)
-        let (l1,l2) = splitAndSortList testData
+        let (l1,l2) = splitAndSortList input 
         Assert.Equivalent(1, l1 |> List.head)
         Assert.Equal(11, findDistance (l1,l2))
         Assert.Equal(31, countOccurrances (l1,l2) |> productOfOccurances)
@@ -44,7 +38,7 @@ module Input =
     let test2 () = 
         let input = readInit "input2.txt" 
         let (l1,l2) = splitAndSortList input 
-        Assert.Equal(2378066, findDistance (l1,l2))
         Assert.Equal(18934359, countOccurrances (l1,l2) |> productOfOccurances) 
+        Assert.Equal(2378066, findDistance (l1,l2))
 
 module Program = let [<EntryPoint>] main _ = 0
