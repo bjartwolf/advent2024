04b19d367d69f0d0cb26ffae3c0c35f0add6791a
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2024-12-01 15:29:12 +0100

    add browser too

--- day_1/day_1.Core/Program.fs.js
+++ day_1/day_1.Core/Program.fs.js
@@ -1,22 +1,26 @@
-import { split } from "../day_1.Node/fable_modules/fable-library-js.4.24.0/String.js";
-import { fold, zip, sumBy, map as map_1, sort, ofArray } from "../day_1.Node/fable_modules/fable-library-js.4.24.0/List.js";
-import { item, map } from "../day_1.Node/fable_modules/fable-library-js.4.24.0/Array.js";
-import { parse } from "../day_1.Node/fable_modules/fable-library-js.4.24.0/Int32.js";
-import { numberHash, comparePrimitives } from "../day_1.Node/fable_modules/fable-library-js.4.24.0/Util.js";
-import { tryFind, ofList } from "../day_1.Node/fable_modules/fable-library-js.4.24.0/Map.js";
-import { List_countBy } from "../day_1.Node/fable_modules/fable-library-js.4.24.0/Seq2.js";
-import { defaultArg } from "../day_1.Node/fable_modules/fable-library-js.4.24.0/Option.js";
+import { split as split_1 } from "../day_1.Browser/fable_modules/fable-library-js.4.24.0/String.js";
+import { fold, zip, sumBy, sort, map as map_1, ofArray } from "../day_1.Browser/fable_modules/fable-library-js.4.24.0/List.js";
+import { item, map } from "../day_1.Browser/fable_modules/fable-library-js.4.24.0/Array.js";
+import { parse } from "../day_1.Browser/fable_modules/fable-library-js.4.24.0/Int32.js";
+import { numberHash, comparePrimitives } from "../day_1.Browser/fable_modules/fable-library-js.4.24.0/Util.js";
+import { tryFind, ofList } from "../day_1.Browser/fable_modules/fable-library-js.4.24.0/Map.js";
+import { List_countBy } from "../day_1.Browser/fable_modules/fable-library-js.4.24.0/Seq2.js";
+import { defaultArg } from "../day_1.Browser/fable_modules/fable-library-js.4.24.0/Option.js";
 
 export function splitOnNewlines(input) {
-    return split(input, ["\r", "\n"], undefined, 1);
+    return split_1(input, ["\r", "\n"], undefined, 1);
 }
 
 export function parseInput(input) {
     let array_1;
-    return ofArray(map((strings) => [parse(item(0, strings), 511, false, 32), parse(item(1, strings), 511, false, 32)], (array_1 = map((line) => split(line, ["  "], undefined, 0), splitOnNewlines(input)), array_1.filter((line_1) => (line_1.length > 0)))));
+    return ofArray(map((strings) => [parse(item(0, strings), 511, false, 32), parse(item(1, strings), 511, false, 32)], (array_1 = map((line) => split_1(line, ["  "], undefined, 0), splitOnNewlines(input)), array_1.filter((line_1) => (line_1.length > 0)))));
 }
 
-export function splitAndSortList(pairList) {
+export function split(pairList) {
+    return [map_1((tuple) => tuple[0], pairList), map_1((tuple_1) => tuple_1[1], pairList)];
+}
+
+export function splitAndSort(pairList) {
     return [sort(map_1((tuple) => tuple[0], pairList), {
         Compare: comparePrimitives,
     }), sort(map_1((tuple_1) => tuple_1[1], pairList), {
@@ -54,12 +58,12 @@ export function productOfOccurances(counts) {
 }
 
 export function distance(input) {
-    const tupledArg = splitAndSortList(parseInput(input));
+    const tupledArg = splitAndSort(parseInput(input));
     return findDistance(tupledArg[0], tupledArg[1]) | 0;
 }
 
 export function productOfInput(input) {
     let tupledArg;
-    return productOfOccurances((tupledArg = splitAndSortList(parseInput(input)), countOccurrances(tupledArg[0], tupledArg[1])));
+    return productOfOccurances((tupledArg = split(parseInput(input)), countOccurrances(tupledArg[0], tupledArg[1])));
 }
 
