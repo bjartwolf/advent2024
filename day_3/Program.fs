be838f9886fd5d4cf24bf55189a39433cb75dac6
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2024-12-03 06:58:39 +0100

    hardcoded for one loop for part2

--- day_3/Program.fs
+++ day_3/Program.fs
@@ -10,6 +10,7 @@ module Input =
 
         txt
 
+
     // regex - mul(X,Y), where X and Y are each 1-3 digit numbers
     let findMatches(input: string) : (int*int) seq =
         let pattern = @"mul\((?<num1>\d{1,3}),(?<num2>\d{1,3})\)"
@@ -24,10 +25,18 @@ module Input =
     let product (input: (int*int) seq): int64 =
         input |> Seq.map (fun (x,y) -> int64 (x * y)) |> Seq.sum
 
+    let processWithDoAndDont (input: string) : int64 =
+        // assume we start with do
+        // split out don't
+        let txt = input.Split("don't")
+        let foo1 = txt.[0] |> findMatches |> product
+        let foo2 = txt.[1].Split("do()")[1] |> findMatches |> product
+        foo1 + foo2 
+
     [<Fact>]
     let test2 () = 
         let input = readInit "input1.txt" 
-        Assert.Equal(71, input.Length) 
+        Assert.Equal(73, input.Length) 
         Assert.Equal(4, input |> findMatches |> Seq.length) 
         Assert.Equal((2,4), input |> findMatches |> Seq.toList |> Seq.head) 
         Assert.Equal(161L, input |> findMatches |> product)
@@ -35,6 +44,6 @@ module Input =
         Assert.Equal(19723, input2.Length) 
         Assert.False(input.Contains(System.Environment.NewLine)) 
         Assert.Equal(184511516L, input2 |> findMatches |> product)
-
+        Assert.Equal(48L, input |> processWithDoAndDont)
 
 module Program = let [<EntryPoint>] main _ = 0
