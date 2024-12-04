60351ff33598c13aa064df162415b25a4d3b3dca
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2024-12-04 06:22:49 +0100

    lines

--- day_4/Program.fs
+++ day_4/Program.fs
@@ -3,15 +3,43 @@ module Input =
     open System.IO
     open Xunit 
 
-    let readInit (filePath: string): int list = 
-        use sr = new StreamReader (filePath) 
-        let line = sr.ReadLine()
-        let numbers = line.Split(",")
-        numbers |> Array.map(fun f -> Int32.Parse(f)) |> Array.toList
+    let readInit (filePath: string): string list = 
+        File.ReadAllLines(filePath) |> Array.toList
+    
+    let createPertubations (wordLength: int) (input: string list): string seq = 
+        let n = input.Length
+        [for line in [0 .. (n - 1)] do
+            // horiztonal lines
+            yield input.[line]
+
+            // vertical lines
+            let vertical = System.String.Concat([0 .. (n-1)] |> List.map (fun i -> input.[i].[line]) )
+            yield vertical 
+        ]
+
+
+        // horizontal lines
+
+
+    // create all lines
+    // there is all vertical lines
+    // all horizontal lines
+    // all downward diagonal lines
+    // all upward diagonal lines
+    
+    // then all of them reversed
+    // so vertical lines reverse
+    // horizontal lines reversed
+    // etc
+
+    // then scan for the word in all the lines
 
     [<Fact>]
     let test2 () = 
-        let input = readInit "input1.txt" 
-        Assert.Equal(1, input.Length) 
+        let inputTest = ["abcde"; "fghij"; "klmno"; "pqrst"; "uvwxy"]
+        printf "%A" (inputTest |> createPertubations 4 |> List.ofSeq)
+//        let input = readInit "input1.txt" 
+        //printf "%A" (input |> createPertubations 4 |> List.ofSeq)
+        //Assert.Equal(1, input.Length) 
 
 module Program = let [<EntryPoint>] main _ = 0
