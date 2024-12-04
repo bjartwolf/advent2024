10584eafb74919ec8f344273c1fff50247dc78e2
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2024-12-04 06:52:28 +0100

    Update Program.fs

--- day_4/Program.fs
+++ day_4/Program.fs
@@ -21,14 +21,17 @@ module Input =
         [for line in [(-n+1) .. (n - 1)] do
             let stringLength = n - abs line
             printfn "%A %A" stringLength line
+            (*
             if line = 0 then
                 yield [0 .. stringLength - 1] |> List.map (fun i -> input.[i].[i]) |> String.Concat
             if line > 0 then
                 yield [0 .. stringLength - 1] |> List.map (fun i -> input.[i].[i + line]) |> String.Concat
             if line < 0 then
                 yield [0 .. stringLength - 1] |> List.map (fun i -> input.[i+(-line)].[i]) |> String.Concat
-
-
+*)
+            if line = 0 then
+                yield [0 .. stringLength - 1] |> List.map (fun i -> input.[stringLength - i - 1].[i]) |> String.Concat
+ 
 //            let dVertical = [0 .. stringLength - 1] 
  //                                   |> List.map (fun i -> input.[i].[i])  
   //                                  |> String.Concat
