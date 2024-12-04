0179d5826b0bc7b78d7817b4344a328268a72fa2
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2024-12-04 07:14:28 +0100

    Update Program.fs

--- day_4/Program.fs
+++ day_4/Program.fs
@@ -2,6 +2,7 @@ module Input =
     open System
     open System.IO
     open Xunit 
+    open System.Text.RegularExpressions
 
     let readInit (filePath: string): string list = 
         File.ReadAllLines(filePath) |> Array.toList
@@ -20,30 +21,37 @@ module Input =
         let n = input.Length
         [for line in [(-n+1) .. (n - 1)] do
             let stringLength = n - abs line
-            printfn "%A %A" stringLength line
-            (*
             if line = 0 then
                 yield [0 .. stringLength - 1] |> List.map (fun i -> input.[i].[i]) |> String.Concat
             if line > 0 then
                 yield [0 .. stringLength - 1] |> List.map (fun i -> input.[i].[i + line]) |> String.Concat
             if line < 0 then
                 yield [0 .. stringLength - 1] |> List.map (fun i -> input.[i+(-line)].[i]) |> String.Concat
-*)
             if line = 0 then
                 yield [0 .. stringLength - 1] |> List.map (fun i -> input.[stringLength - i - 1].[i]) |> String.Concat
             if line > 0 then
                yield [0 .. stringLength - 1] |> List.map (fun i -> input.[stringLength - i - 1 + line].[i + line]) |> String.Concat
             if line < 0 then
-                //printfn "%A" (line )
                 yield [0 .. stringLength - 1] |> List.map (fun i -> input.[line + n - 1 - i].[i]) |> String.Concat
-//            let dVertical = [0 .. stringLength - 1] 
- //                                   |> List.map (fun i -> input.[i].[i])  
-  //                                  |> String.Concat
-   //         yield dVertical
-            //yield line |> string
         ]
 
+    let countOccurrences (input: string): int =
+        let pattern = Regex.Escape("XMAS")
+        Regex.Matches(input, pattern).Count
 
+    let reverseString (input: string): string =
+        new string(input.ToCharArray() |> Array.rev)
+
+    let createAllPertubations (input: string list): string list =
+        let pertubations1 = input |> createPertubations |> List.ofSeq
+        let pertubations2 = input |> createPertubations2 |> List.ofSeq
+        let rev1 = pertubations1 |> List.map reverseString 
+        let rev2 = pertubations2 |> List.map reverseString 
+        [pertubations1; pertubations2; rev1; rev2] |> List.concat
+
+
+    let findOccurances (input: string list) : int =
+        input |> List.map countOccurrences |> List.sum
 
         // horizontal lines
 
@@ -63,14 +71,12 @@ module Input =
 
     [<Fact>]
     let test2 () = 
-        let inputTest = ["abcde"; 
-                         "fghij"; 
-                         "klmno"; 
-                         "pqrst"; 
-                         "uvwxy"]
-        //printf "%A" (inputTest |> createPertubations  |> List.ofSeq)
-        printf "%A" (inputTest |> createPertubations2  |> List.ofSeq)
-//        let input = readInit "input1.txt" 
+//        let inputTest = ["abcde"; "fghij"; "klmno"; "pqrst"; "uvwxy"] //printf "%A" (inputTest |> createPertubations  |> List.ofSeq)
+        //printf "%A" (inputTest |> createAllPertubations |> List.ofSeq)
+        let input = readInit "input1.txt" 
+        Assert.Equal(18, input |>createAllPertubations |> findOccurances) 
+        let input = readInit "input2.txt" 
+        Assert.Equal(18, input |>createAllPertubations |> findOccurances) 
         //printf "%A" (input |> createPertubations  |> List.ofSeq)
         //Assert.Equal(1, input.Length) 
 
