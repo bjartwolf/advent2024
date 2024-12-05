51fe1307b3058a8c8eb4d132418e7c8a34f64676
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2024-12-05 06:37:53 +0100

    forting

--- day_5/Program.fs
+++ day_5/Program.fs
@@ -3,6 +3,7 @@ module Input =
     open System.IO
     open Xunit 
 
+
     let parseRule (rule: string): int*int =
         let splitRule = rule.Split("|", StringSplitOptions.RemoveEmptyEntries)
         (int splitRule.[0], int splitRule.[1])
@@ -16,9 +17,25 @@ module Input =
                                         |> Array.map (fun x -> x |> Array.map int |> Array.toList) |> Array.toList 
         (rules , lists) 
 
+    let numIsInOrder (rules: (int*int) list)  (num: int) (numbers: int list) : bool = 
+        numbers |> List.forall (fun numberToCheck -> rules |> List.exists (fun (ruleX,ruleY) -> ruleX = num && ruleY = numberToCheck))
+
+
+    let rec isSorted (rules: (int*int) list) (list: int list) = 
+        match list with
+            | h :: t -> numIsInOrder rules h t && isSorted rules t
+            | _ -> true
+
     [<Fact>]
     let test2 () = 
         let (rules, lists ) = readInit "input1.txt" 
         Assert.Equal(21, rules.Length) 
+        Assert.True(numIsInOrder rules (List.head lists.[0])  (List.tail lists.[0]))
+        Assert.True(isSorted rules lists.[0]) 
+        Assert.True(isSorted rules lists.[1]) 
+        Assert.True(isSorted rules lists.[2]) 
+        Assert.False(isSorted rules lists.[3]) 
+        Assert.False(isSorted rules lists.[4]) 
+        Assert.False(isSorted rules lists.[4]) 
 
 module Program = let [<EntryPoint>] main _ = 0
