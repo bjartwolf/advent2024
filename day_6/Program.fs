7b081dfcf60134d520bb79750e6c16c9e2e981b4
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2024-12-06 06:20:14 +0100

    parse board

--- day_6/Program.fs
+++ day_6/Program.fs
@@ -3,15 +3,50 @@ module Input =
     open System.IO
     open Xunit 
 
-    let readInit (filePath: string): int list = 
-        use sr = new StreamReader (filePath) 
-        let line = sr.ReadLine()
-        let numbers = line.Split(",")
-        numbers |> Array.map(fun f -> Int32.Parse(f)) |> Array.toList
+    type Position = int *int
+    type Tile = Free | Visited | Obstacle 
+    type Direction = W | N | E | S 
+    type World = Map<Position, Tile>
+
+    let readInit (filePath: string): string [] = 
+        IO.File.ReadAllLines(filePath)
+
+    let parsePipeMap (mapinput: string[]): (World*Position)=
+            let obstacles: (Position*Tile) list = [
+                for i in 0 .. (mapinput.Length - 1) do
+                    let chars = mapinput[i].ToCharArray()
+                    for charIndex in 0 .. (chars.Length - 1) do
+                        let char = chars[charIndex]
+                        match char with 
+                              | '#' -> yield ((i,charIndex), Obstacle)
+                              | _ -> yield ((i,charIndex),Free)
+            ]
+            let startPosition = [
+                for i in 0 .. (mapinput.Length - 1) do
+                    let chars = mapinput[i].ToCharArray()
+                    for charIndex in 0 .. (chars.Length - 1) do
+                        let char = chars[charIndex]
+                        match char with 
+                              | '^' -> yield (i,charIndex)
+                              | _ -> () 
+            ] 
+            let pipeMap = Map.ofList obstacles 
+            (pipeMap, startPosition.Head)
+
+
+    let move (direction: Direction) ((y,x): Position): Position  =
+        match direction with 
+            | N -> (y-1,x)
+            | W -> (y,x-1)
+            | E -> (y,x+1)
+            | S -> (y+1,x)
+
 
     [<Fact>]
     let test2 () = 
-        let input = readInit "input1.txt" 
-        Assert.Equal(1, input.Length) 
+        let world, position = "input1.txt" |> readInit |> parsePipeMap
+        Assert.Equal((6,4), position) 
+        Assert.Equal(Obstacle, Map.find (0,4) world) 
+        Assert.Equal(Free, Map.find (0,3) world) 
 
 module Program = let [<EntryPoint>] main _ = 0
