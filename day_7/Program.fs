f9df428a1202c8a39a1a77ce02f9abd1b4e615e2
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2024-12-07 10:08:59 +0100

    Update Program.fs

--- day_7/Program.fs
+++ day_7/Program.fs
@@ -14,10 +14,26 @@ module Input =
             |> List.map (fun line -> line.Split(":"))
             |> List.map (fun parts -> parts.[0] |> int, parseLine parts.[1]) 
 
+    type Line = int * int list
+    let possibleValues ((num, nums): Line) : int seq =
+        let rec possibleValuesInner (res: int) ((num, nums): Line) : int seq =
+            [ 
+                match nums with
+                    | [] -> yield res
+                    | h::t -> yield! possibleValuesInner (res + h ) (num, t)
+                              yield! possibleValuesInner (res * h ) (num, t)
+            ]
+        possibleValuesInner (List.head nums) (num, List.tail nums)
+
+    [<Fact>]
+    let possibles () = 
+        Assert.Contains(190, possibleValues (190, [10; 19]) |> Seq.toList)
+        Assert.Contains(29, possibleValues (190, [10; 19]) |> Seq.toList)
+        Assert.Contains(3267, possibleValues (3267, [81; 40;27]) |> Seq.toList)
+ 
     [<Fact>]
     let test2 () = 
         let input = readInit "input1.txt" 
-        printfn "%A" input
         Assert.Equal(9, input.Length) 
 
 module Program = let [<EntryPoint>] main _ = 0
