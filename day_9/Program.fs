3b88d1c1ad9f1253706a6d15b9e74b9e7d74cf37
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2024-12-09 22:36:14 +0100

    greej test

--- day_9/Program.fs
+++ day_9/Program.fs
@@ -49,54 +49,34 @@ module Input =
                               inputArray.[indexOfNum.Value] <- None
         result[0 .. numberOfNums - 1] |> Array.toList
 
+    let findSliceIndex (slice: int option []) (array: int option []) =
+        let sliceLength = slice.Length
+        array
+        |> Seq.windowed sliceLength
+        |> Seq.mapi (fun i window -> if window = slice then Some i else None)
+        |> Seq.choose id
+        |> Seq.tryHead
+
+
     let compact2 (input: (int option) list) : (int option) list =
         let inputArray = input |> List.toArray
         let n = input |> List.length
-        let result: int option [] = Array.zeroCreate inputArray.Length 
 
-        let mutable i = 0
-        while i < (input.Length - 1) do
+        let mutable i = n - 1
+        while i > 0 do
             match inputArray.[i] with
-                | Some x -> result.[i] <- Some x 
-                            i <- i + 1
-                | None -> // find how many dotes there are, max 10
-                          let numberOfDots = inputArray[i .. i + 10] |> Array.takeWhile (fun x -> Option.isNone x) |> Array.length
-                          // find something that fits the dots
-                          // need to find consequtive numbers from the back that can fit the dots
-                          let mutable foundSlice:int option []= [||]
-                          let mutable found = false
-                          let mutable j = n - 1
-                          if numberOfDots > 0 then
-                              while j > i && not found && j > 0 do 
-                                  // check to find all equal numbers in a row that fits and skip
-                                  // the size of that numbers if there are numbers that does not fit 
-                                  let value = inputArray.[j]
-                                  match value with 
-                                    | None -> j <- j - 1
-                                    | Some x -> 
-                                      // find all xes in a row at the end of the array
-                                      let xs = inputArray.[j - 10 .. j] |> Array.filter (fun y -> y = Some x)
-                                      if xs.Length <= numberOfDots then
-                                          //let slice = inputArray[n - numberOfDots - j .. n - j] 
-                                          if (xs.Length <= numberOfDots) then
-                                                  foundSlice <- xs 
-                                                  found <- true
-                                      j <- j - xs.Length 
-                          try
-                             if (i + numberOfDots + 1 < inputArray.Length ) && found then 
-                                result[i  .. i + foundSlice.Length - 1] <- foundSlice 
-                                inputArray[j + 1  .. j + foundSlice.Length] <- Array.create foundSlice.Length None
-                          with 
-                             | err -> 
-//                                printfn "%A %A %A" i numberOfDots foundSlice 
-                                printfn "Error: %A" err.Message
-                          if found then
-                              i <- i + 1//numberOfDots//foundSlice.Length 
-                          else 
-                              i <- i + 1
-            printPatternA result |> printfn " RES   %A" 
+                | None -> i <- i - 1 
+                | Some x -> 
+                          let xs = inputArray.[i - 10 .. i] |> Array.rev |> Array.takeWhile (fun y -> y = Some x)
+                          let requiredDots = xs.Length
+                          let dotsArray = Array.replicate requiredDots None
+                          match findSliceIndex dotsArray inputArray[0 .. i]  with 
+                            | Some index -> inputArray.[index .. index + requiredDots - 1] <- xs 
+                                            inputArray.[i - requiredDots + 1 .. i] <- dotsArray
+                                            i <- i - xs.Length 
+                            | None -> i <- i - xs.Length 
             printPatternA inputArray |> printfn " input %A" 
-        result |> Array.toList
+        inputArray |> Array.toList
 
 
     let sumIndexed (input: int list) : int64 =
@@ -133,18 +113,11 @@ module Input =
         let input = readInit "input1.txt" 
         Assert.Equal(19, input.Length) 
       
-    //files and free space on the disk. The digits alternate between indicating the length of a file and the length of free space.
-    // FILE - FREE SPCE
-    // is order FILES before rearranging, stari
-
-    // 1 3 5 -> 0 111 222222(5)
-
-
 module Program = 
     open Input
     let [<EntryPoint>] main _ = 
         printfn "Hei"
-        let compacted = "input2.txt" |> readInit |> parseInput |> expandPattern  |> compact
-        printfn "%A" (compacted |> sumIndexed)
+        let compacted = "input2.txt" |> readInit |> parseInput |> expandPattern  |> compact2
+        printfn "%A" (compacted |> sumIndexed2)
         System.Console.ReadKey() |> ignore
         0
