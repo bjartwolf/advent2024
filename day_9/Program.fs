ba6fff1ab85d6e830c0ebaeb93eb213dbff808aa
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2024-12-09 06:37:30 +0100

    parsing and expanding

--- day_9/Program.fs
+++ day_9/Program.fs
@@ -3,15 +3,43 @@ module Input =
     open System.IO
     open Xunit 
 
-    let readInit (filePath: string): int list = 
-        use sr = new StreamReader (filePath) 
-        let line = sr.ReadLine()
-        let numbers = line.Split(",")
-        numbers |> Array.map(fun f -> Int32.Parse(f)) |> Array.toList
+    let example = "12345"
+    let readInit (filePath: string): string = File.ReadAllText(filePath) 
+
+    let parseInput (input: string): int list =
+        input |> Seq.toList|> Seq.map (fun c -> int c - int '0' ) |> Seq.toList
+
+
+    let expandPattern (input: int list) : (int option) list = 
+        let rec expand (input: int list) (idNr: int) = 
+            match input with
+            | [] -> [] 
+            | file::block::xs -> List.replicate file (Some idNr) @  (List.replicate block None) @ expand xs (idNr + 1)
+            | file::[] -> List.replicate file (Some idNr) 
+        expand input 0
+
+    let printPattern (input: int option list): string = 
+        input |> List.map (fun x -> match x with | Some x -> string x | None -> ".") |> String.concat ""
+
+    [<Fact>]
+    let test3 () =
+        let pattern = example |> parseInput |> expandPattern 
+        Assert.Equivalent("0..111....22222", pattern |> printPattern) 
+
 
     [<Fact>]
     let test2 () = 
+        let exampleParsed = parseInput example
+        Assert.Equivalent([1;2;3;4;5], exampleParsed)
+
         let input = readInit "input1.txt" 
-        Assert.Equal(1, input.Length) 
+        Assert.Equal(19, input.Length) 
+      
+    //files and free space on the disk. The digits alternate between indicating the length of a file and the length of free space.
+    // FILE - FREE SPCE
+    // is order FILES before rearranging, stari
+
+    // 1 3 5 -> 0 111 222222(5)
+
 
 module Program = let [<EntryPoint>] main _ = 0
