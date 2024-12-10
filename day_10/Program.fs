2fc5668c6f80787922fc541bca26d1f016bd1c93
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2024-12-10 06:14:27 +0100

    parse

--- day_10/Program.fs
+++ day_10/Program.fs
@@ -3,15 +3,28 @@ module Input =
     open System.IO
     open Xunit 
 
-    let readInit (filePath: string): int list = 
-        use sr = new StreamReader (filePath) 
-        let line = sr.ReadLine()
-        let numbers = line.Split(",")
-        numbers |> Array.map(fun f -> Int32.Parse(f)) |> Array.toList
+    type Position = int * int
+    type Kart = Map<Position, int>
+
+    let readInit (filePath: string): string[] = File.ReadAllLines(filePath)
+    let parseInit (input: string[]) : Kart = 
+        let foo: (Position*int) list = [
+            for i in 0 .. (input.Length - 1) do
+                let chars = input[i].ToCharArray()
+                for charIndex in 0 .. (chars.Length - 1) do
+                    let char = chars[charIndex]
+                    yield ((i,charIndex), Int32.Parse(char.ToString()))
+        ] 
+        foo |> Map.ofList
+
 
     [<Fact>]
     let test2 () = 
         let input = readInit "input1.txt" 
-        Assert.Equal(1, input.Length) 
+        Assert.Equal(8, input.Length) 
+        let mapInput = [|"123"; "456"; "789"|]
+        let map = parseInit mapInput
+        Assert.Equal(5, Map.find (1,1) map)
+        Assert.Equal(9, Map.find (2,2) map)
 
 module Program = let [<EntryPoint>] main _ = 0
