1e2dc245c1703c212ee7e016da52f9032cdeb3ac
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2024-12-11 21:00:15 +0100

    screw this

--- day_11/Program.fs
+++ day_11/Program.fs
@@ -9,12 +9,12 @@ module Input =
         let numbers = File.ReadAllText(filePath).Split " " 
         numbers |> Array.map(fun f -> Int64.Parse(f)) |> Array.toList
 
-    let runGeneration (input: int64 list): int64 list = 
+    let runGeneration1 (input: int64 list): (int64*int64) list = 
         [
             try 
                 for num in input do
                     if num = 0 then
-                        yield 1 
+                        yield (1L,1L) 
                     else if num.ToString().Length % 2 = 0 then
                         let numStr = num.ToString()
                         let half =numStr.Length / 2
@@ -22,66 +22,73 @@ module Input =
                         let firstHalf = numStr.Substring(0, half)
                         let secondHalf = numStr.Substring(half,  half)
  //                       printfn "firstHalf: %A secondHalf: %A" firstHalf secondHalf
-                        yield firstHalf |> int64
-                        yield secondHalf |> int64
+                        yield (firstHalf |> int64,1L)
+                        yield (secondHalf |> int64,1L)
                     else 
-                        yield num * 2024L 
+                        yield (num * 2024L,1L)
             with 
                 | e -> printfn "Error: %A" e.Message
         ]
-    // før eller siden blir alle tallene 0 igjen og da utvikler de seg i et mønster 
-    let runGenNTimes (input: int64 list) (n: int): int64 list = 
-        let mutable result = input
-        let mutable prevGen = 1
-        for i in 1..n do
-            printfn "Unique numbers in generation %A is %A" (i-1) (result |> List.distinct |> List.length)
- //           printfn "Generation %A is %A with diff %A " (i-1) result.Length (result.Length - prevGen)
-//            printfn "Generation %A is %A " (i-1) result.Length 
-//            printfn "Generation %A is %A " (i-1) result
-            prevGen <- result.Length
-            result <- runGeneration result
-        result
-
-    let runGenNTimesN (input: int64 list) (n: int): int64 list = 
-        let mutable result = input
-        let mutable seenAtGeneration: Dictionary<int64,(int [])> = Dictionary<int64, int[]> ()
-        for i in 0..n do
-            //printfn "Generation %A is %A with diff %A " (i-1) result.Length (result.Length - prevGen)
-            //printfn "Generation %A is %A " (i-1) result.Length 
-            let uniqueNumbers = result |> List.filter (fun x  -> seenAtGeneration.ContainsKey(x) |> not) 
-            for number in result do
-                let seen, generation = seenAtGeneration.TryGetValue(number)
-                if seen then 
-                    let newGen = Array.append generation [|i|]
-                    seenAtGeneration.[number] <- newGen
+    let runGenerationN (input: (int64*int64) list): (int64*int64) list = 
+        [
+            try 
+                for num, count in input do
+                    if num = 0 then
+                        yield (1L,count) 
+                    else if num.ToString().Length % 2 = 0 then
+                        let numStr = num.ToString()
+                        let half =numStr.Length / 2
+//                        printfn "num: %A halfLeng %A length %A" numStr half numStr.Length
+                        let firstHalf = numStr.Substring(0, half)
+                        let secondHalf = numStr.Substring(half,  half)
+ //                       printfn "firstHalf: %A secondHalf: %A" firstHalf secondHalf
+                        yield (firstHalf |> int64,count)
+                        yield (secondHalf |> int64,count)
+                    else 
+                        yield (num * 2024L,count)
+            with 
+                | e -> printfn "Error: %A" e.Message
+        ] 
+        
+    // input is a list of pairs of number and counts
+    // should return the same list but with the unique numbers and counts 
+    let sumCounts (input: (int64*int64) list): (int64*int64) list = 
+        let mutable counts : Dictionary<int64,int64> = Dictionary<int64, int64> ()
+        for (number,count) in input do
+                let inList,sum = counts.TryGetValue(number)
+                if inList then 
+                    counts.[number] <- sum + count 
                 else
-                    seenAtGeneration.[number] <- [|i|]
-
-            let nextRes  = runGeneration uniqueNumbers 
-            result <- nextRes 
-        for num in seenAtGeneration.Keys do
-            let seen, generation = seenAtGeneration.TryGetValue(num)
-            printfn "Number %A was seen at generation %A" num generation
-        result
-
+                    counts.[number] <- count 
+        // convert counts to F# list 
+        counts |> Seq.map (fun k -> (k.Key,k.Value)) |> List.ofSeq
 
     [<Fact>]
-    let runGen () = 
-        Assert.Equivalent([253000], runGeneration [125])
-        Assert.Equivalent([253000;1;7], runGeneration [125;17])
-        Assert.Equivalent([253;0;2024;14168], runGeneration [125;17] |> runGeneration)
+    let sumCountstest () = 
+        Assert.Equivalent([(2,3)], sumCounts [(2,1);(2,2)])
+        Assert.Equivalent([(1,1);(2,1)], sumCounts [(1,1);(2,1)])
+        Assert.Equivalent([(1,3)], sumCounts [(1,1);(1,1);(1,1)])
+ 
+    // før eller siden blir alle tallene 0 igjen og da utvikler de seg i et mønster 
+    let runGenNTimesN (input: int64 list) (n: int): int64 list = 
+        let mutable result = input |> runGeneration1
+        for i in 2..(n-1) do
+            result <- runGenerationN result |> sumCounts
+        let foo = result |> sumCounts 
+        foo |> List.map (fun (num,count) -> count)
+
+
  
     [<Fact>]
     let test2 () = 
-        let input = readInit "input1.txt" 
-        Assert.Equal(2, input.Length) 
-        Assert.Equivalent([2097446912;14168;4048;2;0;2;4;40;48;2024;40;48;80;96;2;8;6;7;6;0;3;2], runGenNTimes input 6)
-        Assert.Equivalent(55312, runGenNTimes input 25 |> List.length)
-
         let input = readInit "input2.txt" 
-        Assert.Equal(8, input.Length) 
+        Assert.Equivalent(284973560658514L, runGenNTimesN input 76 |> List.sum)
+        Assert.Equivalent(4, runGenNTimesN input 3 |> List.sum)
+        Assert.Equivalent(55312, runGenNTimesN input 26 |> List.sum)
+//        Assert.Equivalent(22, runGenNTimesN input 6 |> List.sum)
 
-        Assert.Equivalent(239714, runGenNTimes input 25 |> List.length)
+ //       Assert.Equal(8, input.Length) 
+  //      Assert.Equivalent(239714, runGenNTimesN input 25 |> List.sum)
 
 open Input
 open System
