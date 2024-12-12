3e7d20148273b262103f6fd95079f62027c94839
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2024-12-12 07:05:54 +0100

    maybe finding reginos

--- day_12/Program.fs
+++ day_12/Program.fs
@@ -11,11 +11,52 @@ module Input =
     type Region = Set<Tile>
     type Regions = Region list
 
-    let rec findRegions (input: char[][]) : Regions = 
-        let rec findRegions' (input: char[][]) (x:int) (y:int) (region: Regions) : Regions = 
-            []
+    let minChar = 'A' |> int
+    let maxChar = 'Z' |> int
+    let scanned = 'x' |> int
 
-        findRegions' input 0 0 []
+    let isScanned (input: char[][]) (x:int) (y:int) : bool = 
+        if x < 0 || y < 0 || x >= input.Length || y >= input.[0].Length then false
+        else input.[x].[y] |> int = scanned
+
+    let tileType (input: char[][]) (x:int) (y:int) : char option = 
+        if x < 0 || y < 0 || x >= input.Length || y >= input.[0].Length then None 
+        else Some input.[x].[y]  
+
+    let isInScannedRegion (regions: Regions)  ((x,y): Position ): bool = 
+        regions |> List.exists (fun region -> region |> Set.exists (fun ((x',y'),_) -> x = x' && y = y'))
+
+    let rec findRegions (input: char[][]) : Regions = 
+        let rec findRegions' (positionsToScan: Position list)  (regions: Regions) : Regions = 
+            match positionsToScan with
+            | pos :: restPos ->  
+                    // find candidates around it
+                    let (x,y) = pos 
+                    let up = (x,y-1) 
+                    let left = (x-1,y) 
+                    let down = (x,y-1) 
+                    let right = (x+1,y) 
+                    let candidates = [up;left;down;right]
+                    let unScannedCandidates = candidates |> List.filter (fun (x,y) -> not (isScanned input x y))
+                    let tileTypeCurrent = tileType input x y 
+                    match tileTypeCurrent with
+                        | Some tileType -> 
+                            let regionIndex = regions |> List.tryFindIndex (fun region -> region |> Set.contains (pos,tileType) )
+                            match regionIndex with
+                                | None -> findRegions' restPos (Set.singleton (pos, tileType) :: regions)
+                                | Some i -> let newRegion =  regions.[i] |> Set.add (pos,tileType)
+                                            let newRegions = regions |> List.updateAt i newRegion
+                                            findRegions' restPos newRegions 
+                        | None -> findRegions' restPos regions 
+            | [] -> regions
+                 
+
+        let size = input.Length
+        let xs = [0..size-1]
+        let ys = [0..size-1]
+        let positions = [for x in xs do for y in ys do yield (x,y)]
+
+        findRegions' positions []
     // areal og omkrets
 
     [<Fact>]
