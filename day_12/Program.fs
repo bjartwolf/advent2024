cb6be7d7937f3fd4c272742b0b727263075f9bc3
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2024-12-12 06:21:02 +0100

    Update Program.fs

--- day_12/Program.fs
+++ day_12/Program.fs
@@ -3,15 +3,27 @@ module Input =
     open System.IO
     open Xunit 
 
-    let readInit (filePath: string): int list = 
-        use sr = new StreamReader (filePath) 
-        let line = sr.ReadLine()
-        let numbers = line.Split(",")
-        numbers |> Array.map(fun f -> Int32.Parse(f)) |> Array.toList
+    let readInit (filePath: string): char[][] = 
+        File.ReadAllLines(filePath) |> Array.map (fun x -> x.ToCharArray())
+
+    type Position = int*int
+    type Tile = Position * char
+    type Region = Set<Tile>
+    type Regions = Region list
+
+    let rec findRegions (input: char[][]) : Regions = 
+        let rec findRegions' (input: char[][]) (x:int) (y:int) (region: Regions) : Regions = 
+            []
+
+        findRegions' input 0 0 []
+    // areal og omkrets
 
     [<Fact>]
     let test2 () = 
         let input = readInit "input1.txt" 
-        Assert.Equal(1, input.Length) 
+//        printfn "%A" input
+        let findRegions = findRegions input
+        printfn "%A" findRegions
+        Assert.Equal(10, input.Length) 
 
 module Program = let [<EntryPoint>] main _ = 0
