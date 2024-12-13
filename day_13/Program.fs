36beaf94eb360eeaf031e2f2632f892cf32c8be9
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2024-12-13 06:42:23 +0100

    prase

--- day_13/Program.fs
+++ day_13/Program.fs
@@ -3,15 +3,44 @@ module Input =
     open System.IO
     open Xunit 
 
-    let readInit (filePath: string): int list = 
-        use sr = new StreamReader (filePath) 
-        let line = sr.ReadLine()
-        let numbers = line.Split(",")
-        numbers |> Array.map(fun f -> Int32.Parse(f)) |> Array.toList
+    type Button = { X: int; Y: int}
+    type Price = { GX: int; GY: int}
+
+    type Machine = { A: Button; B: Button; P: Price }
+
+    let parseButton (line: string): Button =
+            let button1 = line.Split("+")
+            let a = button1[1].Split(",")
+            { X = (a.[0] |> int); Y = button1.[2] |> int }
+
+    let parsePrice (line: string): Price =
+            let button1 = line.Split("=")
+            let a = button1[1].Split(",")
+            { GX = a.[0] |> int; GY = button1.[2] |> int }
+
+    [<Fact>]
+    let parseButtonTest () = 
+        let input = "Button A: X+94, Y+34"
+        let button = parseButton input
+        Assert.Equivalent({X = 94; Y= 34}, button)
+
+    let readInit (filePath: string): Machine list = 
+        let file = File.ReadAllText(filePath)
+        let machines = file.Split(sprintf "%s%s" Environment.NewLine  Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+        [
+        for line in machines do
+            let lines = line.Split(sprintf "%s" Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+            let a = parseButton lines.[0]
+            let b = parseButton lines.[1]
+            let c = parsePrice lines.[2]
+            yield { A = a; B=b; P=c } 
+        ] 
+
 
     [<Fact>]
     let test2 () = 
         let input = readInit "input1.txt" 
-        Assert.Equal(1, input.Length) 
+        Assert.Equal(4, input.Length) 
+        Assert.Equal({ A = { X = 94; Y=34}; B = { X = 22; Y = 67}; P = { GX = 8400; GY = 5400 }} , input.Head) 
 
 module Program = let [<EntryPoint>] main _ = 0
