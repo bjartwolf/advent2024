2113528e6205e37005bf9a78f2fa426be07ede42
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2024-12-13 15:31:30 +0100

    Update Program.fs

--- day_13/Program.fs
+++ day_13/Program.fs
@@ -57,53 +57,24 @@ module Solver =
     let costP (p: Presses) : int64 =
         3L*p.PA + p.PB
 
-    let PaFromPb (machine: Machine)(Pb: int64): Presses option = 
-        let Pad = ((double machine.P.GX-(double machine.P.GY*double machine.B.X/double machine.B.Y)))/(double machine.A.X - ((double machine.A.Y*double machine.B.X)/double machine.B.Y))
-        let PadRounded = Math.Round(Pad, 5)
-        let epsilon = 1e-5
-        if Math.Abs(Pad - PadRounded) < epsilon then
-            Some { PA = int64 PadRounded; PB = Pb }
-        else
-            None
-
-    let solveMagic (machine: Machine) : (double*double) = 
+    let solveMagic (machine: Machine) : Presses option = 
         let a = ((double machine.P.GX-(double machine.P.GY*double machine.B.X/double machine.B.Y)))/(double machine.A.X - ((double machine.A.Y*double machine.B.X)/double machine.B.Y))
         let b = (double machine.P.GY) / (double machine.B.Y) - a * (double machine.A.Y) / (double machine.B.Y) 
-        (a,b)
-
-    let generatePresses (machine: Machine) (maxPress: int64): Presses seq =
-        [
-            let pressMachine = PaFromPb machine
-            for j in 0L..100L do
-                 let res = pressMachine j 
-                 match res with
-                    | None -> ()
-                    | Some res-> res 
-        ]
-    let solve (machine: Machine) (maxPresses: int64) : (Presses*int64) option =
-        let checkPressForMachine = checkPresses machine
-        let solutions = generatePresses machine 100 |> Seq.filter(fun p -> checkPressForMachine p) 
-        if (Seq.isEmpty solutions) then 
-            None
+        let aRounded = Math.Round(a, 0) 
+        let bRounded = Math.Round(b, 0)
+        let epsilon = 1e-2
+        if Math.Abs(a- aRounded) < epsilon && Math.Abs(b-bRounded) < epsilon then
+            Some { PA = int64 aRounded; PB = int64 bRounded}
         else
-            let cheapest = 
-                solutions 
-                    |> Seq.map(fun p -> (p, costP p))
-                    |> Seq.minBy (fun (_, c) -> c) 
-            Some cheapest
+            None
+
 
     let solveMachines (machine: Machine list) : ((Presses*int64) option) seq =
         let maxPresses = increase 
         [ for m in machine do
-            match solve m maxPresses with
-            | Some (solution,cost) -> 
-                printfn "*******"
-                printfn "%A" m
-                let magic = solveMagic m 
-                printfn "magic %A" magic 
-                printfn "solution %A" solution
-                
-                yield Some (solution, cost)
+            match solveMagic m with
+            | Some solution -> 
+                yield Some (solution, solution.PA*3L + solution.PB |> int64)
             | None -> 
                 yield None ]
 
@@ -127,12 +98,10 @@ module Tests =
         Assert.False(checkPresses machine0 { PA = 1; PB = 1 } )
         Assert.True(checkPresses machine0 { PA = 80; PB = 40 })
         Assert.True(checkPresses machines.[2] { PA = 38; PB = 86 })
-        Assert.Equal(Some {PA = 80; PB = 40}, PaFromPb machine0 40)
-        Assert.Equal(Some {PA = 38; PB = 86}, PaFromPb machines.[2] 86)
         Assert.Equal(280L, costP { PA = 80; PB = 40 } )
         let solutions = solveMachines machines |> Seq.toList
-        Assert.Equivalent(Some ({ PA = 80; PB = 40 }, 280), solutions.[0])
         Assert.Equivalent(None, solutions.[1])
+        Assert.Equivalent(Some ({ PA = 80; PB = 40 }, 280), solutions.[0])
         Assert.Equivalent(Some ({ PA = 38; PB = 86 }, 200), solutions.[2])
         Assert.Equivalent(None, solutions.[3])
         Assert.Equivalent(480, costOfAllMachines machines)
@@ -140,12 +109,13 @@ module Tests =
     [<Fact>]
     let test3 () = 
         let machines = readInit "input2.txt" 
-        Assert.Equivalent(36250, costOfAllMachines machines)
+        Assert.Equivalent(36250, costOfAllMachines machines )
+        Assert.Equivalent(83232379451012L, costOfAllMachines (machines |> List.map increaseXY) )
 
 module Program = 
     let [<EntryPoint>] main _ = 
         let machines = readInit "input2.txt" 
-        let expensiveMachines = machines //|> List.map increaseXY
+        let expensiveMachines = machines |> List.map increaseXY
         printfn "answer: %A " (costOfAllMachines expensiveMachines)
         Console.ReadLine() |> ignore
         0
