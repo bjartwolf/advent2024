693310cb7206ec9bd1968798170d1e33b3c4ae48
Bjørn Einar Bjartnes <88324093+bjorn-einar-bjartnes-4ss@users.noreply.github.com>
2024-12-19 14:17:17 +0100

    part1

--- day_19/Program.fs
+++ day_19/Program.fs
@@ -20,7 +20,8 @@ module Advent =
         if hits = [] then None
         else Some hits
 
-    let designMatchPatterns (patterns: string list) (nomatch: Set<string> ref) (design: string) : bool =
+    let designMatchPatterns (patterns: string list) (design: string) : bool =
+        let mutable nomatch = Set.empty 
         let matchingPatternStripper = stripMatchingPatterns patterns 
         printfn "Checking %A" design
         let rec designMatchPatterns' (design: string) : bool =
@@ -34,23 +35,22 @@ module Advent =
                     match matchedDesign with 
                         | [] -> false
                         | h :: t ->  
-                                if nomatch.Value.Contains(h) then
+                                if nomatch.Contains(h) then
                                     false
                                 else 
                                     let result = designMatchPatterns' h
                                     printfn "checking %A is %A" h result
                                     if not result then 
-                                        nomatch.Value <- (nomatch.Value).Add(h) 
+                                        nomatch<- nomatch.Add(h) 
                                         t |> List.exists (fun d -> designMatchPatterns' d) 
                                     else 
                                         result 
         designMatchPatterns' design
              
     let countPossibleDesigns (patterns: string list) (designs: string list) : int =
-        let nomatch = ref Set.empty 
         designs 
             |> List.sortDescending
-            |> List.filter (designMatchPatterns patterns nomatch ) |> List.length
+            |> List.filter (designMatchPatterns patterns ) |> List.length
 
 
     [<Fact>]
@@ -65,14 +65,14 @@ module Advent =
         Assert.Equal(0, countPossibleDesigns ["r"] ["rb"])
         Assert.Equal(0, countPossibleDesigns ["rb"] ["r"])
         Assert.Equal(1, countPossibleDesigns ["r"] ["rb";"r"])
-        Assert.True(designMatchPatterns input1 (ref Set.empty) "brwrr")
-        Assert.True(designMatchPatterns input1 (ref Set.empty) "bggr")
-        Assert.True(designMatchPatterns input1 (ref Set.empty) "gbbr")
-        Assert.True(designMatchPatterns input1 (ref Set.empty) "rrbgbr")
-        Assert.False(designMatchPatterns input1 (ref Set.empty) "ubwu")
-        Assert.True(designMatchPatterns input1 (ref Set.empty) "bwurrg")
-        Assert.True(designMatchPatterns input1 (ref Set.empty) "brgr")
-        Assert.False(designMatchPatterns input1 (ref Set.empty) "bbrgwb")
+        Assert.True(designMatchPatterns input1 "brwrr")
+        Assert.True(designMatchPatterns input1 "bggr")
+        Assert.True(designMatchPatterns input1 "gbbr")
+        Assert.True(designMatchPatterns input1 "rrbgbr")
+        Assert.False(designMatchPatterns input1 "ubwu")
+        Assert.True(designMatchPatterns input1 "bwurrg")
+        Assert.True(designMatchPatterns input1 "brgr")
+        Assert.False(designMatchPatterns input1 "bbrgwb")
         Assert.Equal(6, countPossibleDesigns input1 designs1)
         let input2,designs2 = get_input "input2.txt"
         Assert.NotEqual(307, countPossibleDesigns input2 designs2)
